b79d9de752849bde82bca75a567dd8e4f7bd0c9f
Abhinavcv2002 <abhinavcv227@gmail.com>
2025-04-11 16:01:24 +0530

    mounting methods

--- React-demo/src/App.jsx
+++ React-demo/src/App.jsx
@@ -42,24 +42,185 @@ import React from 'react'
 // }
 
 // -----------age changing----------- //
+
+// class App extends React.Component {
+//   constructor(){
+//     super()
+//     this.state = {age:21}
+//   }
+//   changeAge = () => {
+//     this.setState({age:25})
+//   }
+//   render(){
+//     return(
+//     <>
+//     <h1>hello</h1>
+//     <h2>age {this.props.age} </h2>
+//     <button onClick={
+//       this.changeAge
+//     }>change Age</button>
+//     </>
+//     )
+//   }
+// }
+
+// --------------------mounting (getderivedStatefromprops)------------------//
+
+// class App extends React.Component {
+//   constructor(props){
+//     super(props)
+//     this.state = {age:20}
+//   }
+//   changeAge = () => {
+//     this.setState({age:25})
+//   }
+
+//   static getDerivedStateFromProps(props,state){
+//     return {age:props.age}
+//   }
+
+//   render(){
+//     return(
+//     <>
+//     <h1>hello</h1>
+//     <h2>age {this.props.age} </h2>
+//     <button onClick={this.changeAge}>change Age</button>
+//     </>
+//     )
+//   }
+// }
+
+// -------------------componentDidMount()---------------------//
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { age: 20 };
+//   }
+
+//   componentDidMount() {
+//     setTimeout(() => {
+//       this.setState({ age: 30 });
+//     }, 1000);
+//   }
+
+//   changeAge = () => {
+//     this.setState({ age: 40 }); // Just an example
+//   }
+
+//   render() {
+//     return (
+//       <>
+//         <h1>Hello</h1>
+//         <h2>Age: {this.state.age}</h2>
+//         <button onClick={this.changeAge}>Change Age</button>
+//       </>
+//     );
+//   }
+// }
+
+// -------------------componentDidMount()---------------------//
+
+// class App extends React.Component {
+//   constructor(props){
+//     super(props);
+//     this.state = { age: 20 };
+//   }
+
+//   shouldComponentUpdate(){
+//     return true;
+//   }
+
+//   changeAge = () => {
+//     this.setState({ age: 30 });
+//   }
+
+//   render(){
+//     return (
+//       <>
+//         <h1>Hello</h1>
+//         <h2>Age: {this.state.age}</h2>
+//         <button onClick={this.changeAge}>Change Age</button>
+//       </>
+//     );
+//   }
+// }
+
+// -------------------getSnapshotBeforeUpdate()---------------------//
+
+// class App extends React.Component {
+//   constructor(props){
+//     super(props);
+//     this.state = { age: 20 };
+//   }
+
+//   changeAge = () => {
+//     this.setState({ age: 30 });
+//   }
+
+//   componentDidMount(){
+//     setTimeout(() => {
+//       this.setState({age:30})
+//     },1000);
+//   }
+
+//   getSnapshotBeforeUpdate(prevprops,prevstate){
+//     console.log(prevprops,prevstate);
+//   }
+
+//   componentDidUpdate(){
+//     console.log("component updated");
+    
+//   }
+//   render(){
+//     return (
+//       <>
+//         <h1>Hello</h1>
+//         <h2>Age: {this.state.age}</h2>
+//         <button onClick={this.changeAge}>Change Age</button>
+//       </>
+//     );
+//   }
+// }
+
+// ------------------------componentWillUnmount()---------------------//
+
 class App extends React.Component {
   constructor(){
-    super()
-    this.state = {age:21}
+    super();
+    this.state = {show:true};
   }
-  changeAge = () => {
-    this.setState({age:25})
+
+  deleteHead = () => {
+    this.setState({ show:false });
   }
+
   render(){
-    return(
-    <>
-    <h1>hello</h1>
-    <h2>age {this.props.age} </h2>
-    <button onClick={
-      this.changeAge
-    }>change Age</button>
-    </>
-    )
+    let myhead;
+    if(this.state.show){
+      myhead = <Sub/>
+    }
+    return (
+      <div>
+        {myhead}
+        <h1>Hello</h1>
+        <button onClick={this.deleteHead}>Delete </button>
+      </div>
+    );
   }
 }
-export default App
+
+class Sub extends React.Component{
+  componentWillUnmount(){
+    alert("componentWillUnmount")
+  }
+  render() {
+    return (
+      <div>
+        <h1>Sub</h1>
+      </div>
+    );
+  } 
+}
+
+export default App;
--- React-demo/src/main.jsx
+++ React-demo/src/main.jsx
@@ -5,6 +5,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App age="21" />
+    <App age="22" />
   </StrictMode>,
 )
